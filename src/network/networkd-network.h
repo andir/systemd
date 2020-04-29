d171a50a67246b1fa8d96a23da529da7503517e2
Andreas Rammhold <andreas@rammhold.de>
2020-04-29 15:09:49 +0200

    WIP introduce dhcp6_get_preferred_delgated_prefix function

--- src/network/networkd-network.h
+++ src/network/networkd-network.h
@@ -173,6 +173,7 @@ struct Network {
 
         /* IPv6 prefix delegation support */
         RADVPrefixDelegation router_prefix_delegation;
+        int32_t router_prefix_subnet_id;
         usec_t router_lifetime_usec;
         uint8_t router_preference;
         bool router_managed;
